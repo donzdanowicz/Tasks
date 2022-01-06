9296161d8af781680bdf28c7dcc7ba5aba5e9920
donzdanowicz <madpz89@gmail.com>
2022-01-06 16:29:51 +0100

    Task 23.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public class Mail {
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+}
+
